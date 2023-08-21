18474a18ca207c061127ebcee9b10a0c5e329395
David Gherghita <DavidGherghita@gmail.com>
2023-08-21 20:34:40 +0300

    Add script for testing performance using perf3

---
+++ iperf3/script.ps1
@@ -0,0 +1,16 @@
+$interfaceAlias = "Ethernet"
+
+$iperfServer = "192.168.0.118"
+$iperfPort = "5201"
+
+$iperfPath = ".\iperf3.exe"
+$outputFile = "iperf_results.txt"
+
+for ($mtu = 70; $mtu -le 1500; $mtu += 10) {
+    for ($i = 0; $i -lt 5; $i++) {
+        netsh interface ipv4 set subinterface "$interfaceAlias" mtu=$mtu store=persistent
+
+        $iperfOutput = & $iperfPath -c $iperfServer -p $iperfPort -f m -i 10
+        $iperfOutput | Out-File -Append -FilePath $outputFile
+    }
+}
